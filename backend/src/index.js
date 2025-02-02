1b3a7e74b7c5db95b4852edf8ce9bffe636ae2af
Amul Op <amulsharmarr67@gmail.com>
2025-02-02 14:04:18 +0530

    UdaanX - setting up routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const {PORT} = require('./config');
-
+const express = require("express");
+const { PORT } = require("./config");
+const apiRoutes = require('./routes');
 
 const app = express();
 
+//registring api routes
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Port running successfully at ${PORT}`);
-})
+  console.log(`Port running successfully at ${PORT}`);
+});
